72084986c9d9211e896fd835805a1f2db7b5093e
Rufus Wong <rcywongaa@gmail.com>
2021-07-15 23:25:32 +0800

    Remove unnecessary joint_state_publisher

--- urdf_visualizer/launch/main.py
+++ urdf_visualizer/launch/main.py
@@ -29,13 +29,6 @@ def generate_launch_description():
             parameters=[{'use_sim_time': False, 'robot_description': robot_desc}],
             arguments=[urdf]),
         Node(
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
-            name='joint_state_publisher',
-            output='screen',
-            parameters=[{'robot_description': robot_desc}],
-            arguments=[]),
-        Node(
             package='joint_state_publisher_gui',
             executable='joint_state_publisher_gui',
             name='joint_state_publisher_gui',
